40b17f1b488dece2b91a83a1ec5b6a3f47e50f2c
Lunatic1 <112961007+TheLunatic1@users.noreply.github.com>
2025-12-09 17:14:45 +0600

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,14 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "gamelibrary_db");
+// InfinityFree Database Connection for GameLibrary Project
+$host = "sql100.infinityfree.com";      // MySQL Host Name
+$username = "if0_40636686";             // MySQL User Name
+$password = "aZavW4oBLw8Vs";            // vPanel password
+$database = "if0_40636686_gamelibrary_db";  // Database name
+
+// Connect to MySQL
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
